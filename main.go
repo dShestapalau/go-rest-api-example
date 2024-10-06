0e13945061efea8d3f6467f16670d0b282a1b6ba
dzianisshestapalau <dzianis.shestapalau@gmail.com>
2024-10-06 21:05:24 +0300

    PUT query

--- main.go
+++ main.go
@@ -1,75 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"shestapalau.by/rest/db"
-	"shestapalau.by/rest/models"
+	"shestapalau.by/rest/routes"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
-	server.GET("/events", GetEvents)
-	server.GET("/events/:id", GetEvent)
-	server.POST("/events", CreateEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
-}
-
-func GetEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
-		return
-	}
-
-	if len(events) == 0 {
-		events = []models.Event{}
-	}
-
-	context.JSON(http.StatusOK, events)
-}
-
-func GetEvent(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
-		return
-	}
-
-	event, err := models.GetEventById(id)
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func CreateEvent(context *gin.Context) {
-	var event models.Event
-
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
-		return
-	}
-
-	event.UserID = 1
-	err = event.Save()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
-		return
-	}
-
-	context.JSON(http.StatusCreated, event)
 }
---
+++ routes/events.go
@@ -0,0 +1,98 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"shestapalau.by/rest/models"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+		return
+	}
+
+	if len(events) == 0 {
+		events = []models.Event{}
+	}
+
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+		return
+	}
+
+	event, err := models.GetEventById(id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+		return
+	}
+
+	event.UserID = 1
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, event)
+}
+
+func updateEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+		return
+	}
+
+	_, err = models.GetEventById(id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+		return
+	}
+
+	var updatedEvent models.Event
+
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+		return
+	}
+
+	updatedEvent.ID = id
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+		return
+	}
+
+	context.JSON(http.StatusOK, updatedEvent)
+}
